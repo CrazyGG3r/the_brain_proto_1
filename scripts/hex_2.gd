93eef598a74a17bd9a3c4db19338d2086b5b404a
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-09-29 18:24:32 +0500

    kadskjaksdjkajdkasj dksajksjk jadlkjasjdsja ldjalskjd asjl

--- scripts/hex_2.gd
+++ scripts/hex_2.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
+@export var move_speed = 5.0
+@export var turn_speed = 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var coxa_min_rot = deg_to_rad(-90)
+var coxa_max_rot = deg_to_rad(90)
+var femur_min_rot = deg_to_rad(-90)
+var femur_max_rot = deg_to_rad(90)
+var tibia_min_rot = deg_to_rad(-90)
+var tibia_max_rot = deg_to_rad(90)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
