32b3abf8f70d38e6d698c4f8a0478abd05921146
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-09-30 12:15:01 +0500

    so much change cant keep track

--- scripts/hex_4.gd
+++ scripts/hex_4.gd
@@ -9,11 +9,11 @@ var femur_min_rot = deg_to_rad(-90)
 var femur_max_rot = deg_to_rad(90)
 var tibia_min_rot = deg_to_rad(-90)
 var tibia_max_rot = deg_to_rad(90)
-@onready var legtest = $Armature/Skeleton3D/L_coxa_B
+@onready var legtest =$Armature/Skeleton3D/L_coxa_B_2
 
 func _ready():
 	pass
 
-func _process(delta):
+func _process(_delta):
 	var target_rotation: float = deg_to_rad(90) * turn_speed / 100
 	legtest.rotation_degrees.x = clamp(target_rotation, rad_to_deg(coxa_min_rot), rad_to_deg(coxa_max_rot))
